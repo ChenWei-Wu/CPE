1fadaf95b9631e532f4ba49cb9b39fb217d27b11
wuchenwei <js.870221@gmail.com>
2019-06-28 00:06:12 +0800

    06/28 00:06

--- Conformity_11286.cpp
+++ Conformity_11286.cpp
@@ -1,7 +1,21 @@
-#include<bits/stdc++.h>
+#include <set>
+#include <iostream>
+#include <map>
 using namespace std;
 
 int main()
 {
-	ios_base::sync_with_stdio(0);
+	int n;
+	cin >> n;
+	while(n != 0)
+	{
+		map<set<int> , int> count;
+		int m = 0 , mc = 0;
+		while(n--)
+		{
+			set<int> courses;
+			cin >> courses;
+			
+		}
+	}	
 }
